a4cc1c28c8c7e6030cc297bbe100ce757b3d22c4
rayenBB <rayen.benboubaker@etudiant-enit.utm.tn>
2023-08-28 14:55:10 +0100

    swagger

--- src/main/java/com/stage/security/config/SecurityConfiguration.java
+++ src/main/java/com/stage/security/config/SecurityConfiguration.java
@@ -47,8 +47,19 @@ public class SecurityConfiguration {
             .csrf()
             .disable()
             .authorizeHttpRequests()
-            .requestMatchers("/api/v1/auth/**")
-            .permitAll()
+            .requestMatchers(
+                    "/api/v1/auth/**",
+                    "/v2/api-docs",
+                    "/v3/api-docs",
+                    "/v3/api-docs/**",
+                    "/swagger-resources",
+                    "/swagger-resources/**",
+                    "/configuration/ui",
+                    "/configuration/security",
+                    "/swagger-ui/**",
+                    "/webjars/**",
+                    "/swagger-ui.html"
+            )            .permitAll()
             .requestMatchers("/user/**").permitAll()
             .requestMatchers("/ao/**").permitAll()
 
